b5d4e9f7f1680fb0c98855d7be0465134eedaa2a
NicolasXs <nsdev14@hotmail.com>
2024-05-06 22:54:48 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import 'dotenv/config';
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GuestsModule } from './guests/guests.module';
+import { PropertyModule } from './property/property.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    GuestsModule,
+    PropertyModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
