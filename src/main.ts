6ff1969d9cfab38d151e1c047768316677408223
NicolasXs <nsdev14@hotmail.com>
2024-05-14 23:57:37 -0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,11 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-import * as cors from 'cors';
-
-import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(
-    cors({
-      origin: true,
-      credentials: true,
-    }),
-  );
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
