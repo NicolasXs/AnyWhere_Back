929e37e5391a5c169904503721b9a42048291172
NicolasXs <nsdev14@hotmail.com>
2024-05-19 21:53:03 -0300

    delete 8 files

--- src/guests/guests.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GuestsService } from './services/guests.service';
-import { GuestsController } from './controllers/guests.controller';
-
-@Module({
-  controllers: [GuestsController],
-  providers: [GuestsService],
-})
-export class GuestsModule {}
